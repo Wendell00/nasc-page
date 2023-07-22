dc8346af6fd72356cfba0785ba37d8bd7b242627
wendell00 <wendellborges2003@gmail.com>
2023-07-22 12:01:23 -0300

    swiper no sobre

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper', {
     autoplay: {
-      delay: 8000,
+      delay: 2000,
       disableOnInteraction: false,
   },
     loop: true,
@@ -15,6 +15,18 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: ["-20%", 0, -1],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  },
   
   });
 
